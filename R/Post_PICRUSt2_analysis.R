33b1f8eb39226c73b2b630dc6b4a13398184ac7e
Victor Jarquin <victor@harriet.biologie.hu-berlin.de>
2020-11-24 20:26:29 +0100

    Heatmaps for enzymes and pathways

--- R/Post_PICRUSt2_analysis.R
+++ R/Post_PICRUSt2_analysis.R
@@ -233,5 +233,124 @@ BCPredDestop25 <- pheatmap(PredDestop25.braycurt,
 #BCPredDestop25
 #dev.off()
 
+####Heat map pathways#####
+##Matrix Pathways sample
+PredPathDesM<- PredPathDes
+rownames(PredPathDesM)<-PredPathDesM$pathway
+PredPathDesM$pathway<- NULL
+PredPathDesM$description<- NULL
+PredPathDesM<- as.matrix(PredPathDesM)
+
+##Select the top 25 more abundant pathways 
+PredPathDes%>%
+  replace(is.na(.), 0)%>%
+  mutate(Total = rowSums(select(., contains("Sample"))))%>%
+  select(pathway, description, Total)%>%
+  column_to_rownames(var = "pathway")-> tmp
+
+tmp <- tmp[order(-tmp$Total), ]
+tmp <- rownames(tmp[1:25, ])
+PredPathDestop25 <- data.frame(PredPathDesM[tmp, ])
+
+df<- data.frame(PredPathDestop25)
+df<- t(df)
+df<- data.frame(df)
+df%>% 
+  mutate(Total= rowSums(select(., contains("PWY"))))%>%
+  mutate_all(.funs = ~./Total)->df
+
+rownames(df)<-rownames(t(data.frame(PredPathDestop25)))
+df$Total<- NULL
+df<- t(df)
+
+mPredPathDestop25 <- data.frame(Pathway = rownames(df), df)
+mPredPathDestop25 <- melt(mPredPathDestop25, id.vars = "Pathway")
+colnames(mPredPathDestop25) <- c("Pathway", "Sample_Name", "Relative_abundance")
+
+##Add metadata 
+mPredPathDestop25<- plyr::join(mPredPathDestop25, coldata, by="Sample_Name")
+
+mPredPathDestop25%>%
+  ggplot(aes(x = reorder(Pathway, -Relative_abundance), y = Relative_abundance, color= Pathway)) +
+  scale_color_viridis_d()+
+  geom_jitter(size = 2, alpha = 0.05, width = 0.2) +
+  stat_summary(fun = mean, geom = "point", size = 5)+
+  coord_flip() +
+  scale_y_continuous(name = "Pathway relative abundance", limits = c(0, 0.1))+
+  labs(tag= "A)")+
+  xlab("Pathway") +
+  theme_bw()+
+  theme(legend.position = "none")+
+  geom_segment(
+    aes(x = Pathway, xend = Pathway,
+        y = 0.04, yend = mean(Relative_abundance)),
+    size = 0.8) +
+  geom_hline(aes(yintercept = 0.04), color = "gray70", size = 0.6)
+  
+mPredPathDestop25%>%
+  filter(Compartment%in%c("Ascaris"))%>%
+  ggplot(aes(y= Relative_abundance, x= reorder(Pathway, -Relative_abundance)))+
+  scale_y_continuous("Pathway relative abundance")+
+  geom_boxplot(aes(color= WormSex))+
+  xlab("Pathway")+
+  labs(tag= "B)")+
+  coord_flip() +
+  theme_bw()+
+  theme(text = element_text(size=16))
+
+mPredPathDestop25%>%
+  filter(Compartment%in%c("Faeces", "Duodenum","Jejunum", "Colon", "Cecum", "Ileum"))%>%
+  ggplot(aes(y= Counts, x= reorder(Pathway, -Counts)))+
+  scale_y_log10("log10 Pathway abundance", 
+                breaks = scales::trans_breaks("log10", function(x) 10^x),
+                labels = scales::trans_format("log10", scales::math_format(10^.x)))+
+  geom_boxplot(aes(color= Compartment))+
+  xlab("Pathway")+
+  scale_color_brewer(palette = "Set3")+
+  labs(tag= "C)")+
+  theme_bw()+
+  theme(text = element_text(size=16), axis.text.x = element_text(angle = 45, hjust = 1))+
+  annotation_logticks(sides = "l")
+
+
+
+###Using pheatmap to include annotations 
+df.matix<-as.matrix(df)
+df.clust <- hclust(dist(df.matix), method = "complete") ##Dendogram
+
+require(dendextend)
+as.dendrogram(df.clust) %>%
+  plot(horiz = TRUE)
+
+df.col <- cutree(tree = df.clust, k = 2)
+df.col  <- data.frame(cluster = ifelse(test = df.col  == 1, yes = "cluster 1", no = "cluster 2"))
+PredDestop25.col$Sample_Name <- rownames(PredDestop25.col)
+
+PredDestop25.col <- merge(PredDestop25.col, coldata, by="Sample_Name", sort= F)
+
+col_groups <- PredDestop25.col %>%
+  select("Sample_Name", "AnimalSpecies", "Compartment") ##Here It is possible to add the expected size 
+
+row.names(col_groups)<- col_groups$Sample_Name
+
+col_groups$Sample_Name<- NULL
+
+colour_groups <- list(AnimalSpecies= c("Pig"= "pink", "Ascaris"= "#C46210"),
+                      Region= c("Faeces"= "#8DD3C7","Colon"= "#009999" ,"Duodenum"= "#FFFFB3", "Jejunum"= "#BEBADA", 
+                                "Negative"= "#FB8072", "Ascaris"= "#80B1D3"))
+
+BCPredDestop25 <- pheatmap(PredDestop25.braycurt, 
+                           color = viridis(100),
+                           border_color = NA,
+                           annotation_col = col_groups, 
+                           #annotation_row = col_groups,
+                           annotation_colors = colour_groups,
+                           #cutree_rows = 2,
+                           #cutree_cols = 2,
+                           show_rownames = F,
+                           show_colnames = F,
+                           main= "Bray-Curtis dissimilarity among samples")
+
+
 ##DESeq analysis
 ddsTable<- DESeq(ddsTable)
